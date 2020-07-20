f5b3434582c849888794e4030de27565d3e983d4
gersilva96 <gersilva96@hotmail.com>
2020-07-20 12:07:52 -0300

    Terminado cronometro

--- 22-Eventos y formularios/02-CronometroConJavaScript/js/functions.js
+++ 22-Eventos y formularios/02-CronometroConJavaScript/js/functions.js
@@ -4,19 +4,25 @@ window.addEventListener("load", () => {
     let resetBtn = document.querySelector(".reset");
     let minutes = document.querySelector(".reloj-parte.minutes");
     let seconds = document.querySelector(".reloj-parte.seconds");
+
+    pauseBtn.setAttribute("disabled", "");
+    resetBtn.setAttribute("disabled", "");
+
     let sec = 0;
     let min = 0;
 
-    pauseBtn.setAttribute("disabled", "");
-
     let crono;
 
-    const escribir = () => {
+    const escribir = (reset) => {
         let secAux, minAux;
         sec++;
         if (sec > 59) {
             min++;
             sec = 0;
+        }
+        if (reset != undefined) {
+            sec = 0;
+            min = 0;
         }
         (sec < 10) ? secAux = `0${sec}` : secAux = `${sec}`;
         (min < 10) ? minAux = `0${min}` : minAux = `${min}`;
@@ -29,20 +35,32 @@ window.addEventListener("load", () => {
         crono = setInterval(escribir, 1000);
         startBtn.removeEventListener("click", cronometrar);
         startBtn.setAttribute("disabled", "");
+        pauseBtn.addEventListener("click", pause);
+        pauseBtn.removeAttribute("disabled");
+        resetBtn.removeEventListener("click", reset);
+        resetBtn.setAttribute("disabled", "");
     };
 
     const pause = () => {
         clearInterval(crono);
-        pauseBtn.removeAttribute("disabled");
         startBtn.addEventListener("click", cronometrar);
+        startBtn.removeAttribute("disabled");
+        pauseBtn.removeEventListener("click", pause);
+        pauseBtn.setAttribute("disabled", "");
+        resetBtn.addEventListener("click", reset);
+        resetBtn.removeAttribute("disabled");
     };
 
-    /*const restart = () => {
+    const reset = () => {
         clearInterval(crono);
-        alert("Restart");
-    };*/
+        escribir(1);
+        startBtn.addEventListener("click", cronometrar);
+        startBtn.removeAttribute("disabled");
+        pauseBtn.removeEventListener("click", pause);
+        pauseBtn.setAttribute("disabled", "");
+        resetBtn.removeEventListener("click", reset);
+        resetBtn.setAttribute("disabled", "");
+    };
 
     startBtn.addEventListener("click", cronometrar);
-    pauseBtn.addEventListener("click", pause);
-    //restartBtn.addEventListener("click", restart);
 });
