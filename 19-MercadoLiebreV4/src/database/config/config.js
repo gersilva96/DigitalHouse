b539b50496dd6cef25c2228f41bba479dc90c13e
German <gersilva96@hotmail.com>
2020-08-04 12:42:39 -0300

    Dejo password genérica

--- 19-MercadoLiebreV4/src/database/config/config.js
+++ 19-MercadoLiebreV4/src/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
-    "username": "ger",
-    "password": "Desarrollo1!",
+    "username": "root",
+    "password": "PASSWORD",
     "database": "mercadoliebre",
     "host": "127.0.0.1",
     "dialect": "mysql",
